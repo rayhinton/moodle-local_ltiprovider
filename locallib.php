7a0a39ec8ac4cb24c4d1a72c31c366c654a288f5
Antoni Bertran <abertranb@uoc.edu>
2019-09-18 15:40:09 +0200

    Deleted var_dump

--- locallib.php
+++ locallib.php
@@ -323,7 +323,7 @@ function local_ltiprovider_get_custom_new_course_info($field, $context) {
 	$customformat = trim(get_config('local_ltiprovider', $field . "formatcustom"));
 	if ($customformat) {
 		$separators  = array(':', ' ');
-		foreach ($separators as $separator) {var_dump($separator);
+		foreach ($separators as $separator) {
 			$customformatarray = explode($separator, $customformat);
 			foreach ($customformatarray as $item) {
 				$value = local_ltiprovider_get_custom_new_course_info_add_param($value, $item, $context, $separator);
